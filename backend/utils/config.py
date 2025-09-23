50aa55a5b3a901b6105f63748bf8d8b173423237
Irina <irbris@mail.ru>
2025-09-23 11:53:31 +0300

    feat: update agent section

--- backend/utils/config.py
+++ backend/utils/config.py
@@ -7,5 +7,4 @@ class Config:
     NOTION_API_KEY = os.getenv('NOTION_API_KEY')
     NOTION_DATABASE_ID = os.getenv('NOTION_DATABASE_ID')
     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-    NOTION_ROOT_PAGE_ID = os.getenv('NOTION_ROOT_PAGE_ID')
     
